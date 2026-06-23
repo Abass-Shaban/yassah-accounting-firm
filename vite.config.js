704ca81c756e2ff5232fe19aeb48ca0555667072
Abass-Shaban <shabanobongita@zetech.ac.ke>
2026-06-23 16:28:55 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.webp', '**/*.jpeg', '**/*.jpg', '**/*.png'],
-  base: '/',
+  base: '/yassah-accounting-firm/',
 })
